e09f4a1f9d8c80a6f5c5de392f2846187cde1b32
Mohamed Hafez <mhafez1978@outlook.com>
2021-06-10 23:48:31 -0400

    first commit here mh

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert('Welcome to my page')
+var myVar;
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+  document.getElementById("loader").style.display = "block";
+  document.getElementById("main").style.display = "none";
+
+
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("main").style.display = "block";
+}
